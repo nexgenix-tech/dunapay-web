607114392f2b7104545ec57c53053f30bd9f74ef
Valiant K <valiant@nexgx.co.za>
2025-07-15 22:10:40 +0200

    fixed pages

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const VehiclesPage: React.FC = () => (
+    <div style={{ textAlign: "center", marginTop: "4rem" }}>
+        <h1>Vehicles Dashboard</h1>
+        <p>This page is not implemented yet. Coming soon!</p>
+    </div>
+);
+
+export default VehiclesPage;
